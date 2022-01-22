f70f75e61b1e888483810dc3ec219ee6edc95723
Attila Kelemen <attila.kelemen85@gmail.com>
2022-01-22 23:42:36 +0100

    Added API doc for JTrim Image 2.0.5.

---
+++ api/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.jtrim2.image","l":"BufferedImages"},{"p":"org.jtrim2.image","l":"ImageMetaData"},{"p":"org.jtrim2.image","l":"ImageResult"},{"p":"org.jtrim2.image","l":"JavaIIOMetaData"}];updateSearchResults();
