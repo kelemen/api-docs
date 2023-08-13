6e1abe9bc4e8f1fcae71838ee33259e36d067bd6
Attila Kelemen <attila.kelemen85@gmail.com>
2023-08-13 04:03:41 +0200

    Added API doc for JTrim Executor 2.0.7.

--- api/package-search-index.js
+++ api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.jtrim2.executor"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"jtrim.executor","l":"org.jtrim2.executor"}];updateSearchResults();
