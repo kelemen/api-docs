39bd6e32176dd7e44bd4c7a8b73397a2bd25d62c
Attila Kelemen <attila.kelemen85@gmail.com>
2023-08-13 04:03:48 +0200

    Added API doc for JTrim Image Query 2.0.7.

--- api/module-search-index.js
+++ api/module-search-index.js
@@ -1,1 +1,1 @@
-moduleSearchIndex = [];updateSearchResults();
+moduleSearchIndex = [{"l":"jtrim.image.query"}];updateSearchResults();
--- api/package-search-index.js
+++ api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.jtrim2.image.async"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"jtrim.image.query","l":"org.jtrim2.image.async"}];updateSearchResults();
